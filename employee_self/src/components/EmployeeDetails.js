a37413df0d5f7b15299660ea0830295b18f614cc
kosalaMaduwantha <52875765+kosalaMaduwantha@users.noreply.github.com>
2022-12-27 23:54:02 +0530

    adding the styles of the details page

--- employee_self/src/components/EmployeeDetails.js
+++ employee_self/src/components/EmployeeDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Row, Col, Container} from 'react-bootstrap'
+import profLogo from '../assets/pro_pic_avatar.jpg'
 
 
 
@@ -8,11 +9,55 @@ function EmployeeDetails(){
 
 
     return(
-    <Container fluid style={{padding:0, overflow:'hidden'}}>  
-    <Row style={{backgroundColor:'red'}}>
-        <Col style={{height:"100vh", backgroundColor:'white'}} xl={2}>
-            <Row>
-                <Container>profile picture</Container>
+    <Container 
+        fluid 
+        style={{
+            padding:0, 
+            overflow:'hidden'
+            }}>
+
+    <Row 
+        style={{
+            backgroundColor:'red'
+            }}>
+
+        <Col 
+            style={{
+                height:"100vh", 
+                backgroundColor:'white'
+                }} 
+            xs={12} 
+            sm={6} 
+            md={4} 
+            lg={3}>
+
+            <Row 
+            className="text-center"
+            style={{
+                paddingLeft:'60px',
+                paddingRight:'60px',
+                paddingTop:'60px'
+            }}>
+                <Container 
+                    className="text-center overflow-hidden"
+                    style={{
+                        borderStyle:'solid',
+                        borderColor:'black',
+                        borderWidth:'2px',
+                        
+                    }}>
+
+                    <img 
+                        src={profLogo}
+                        alt="profile"
+                        style={{
+                            maxWidth:'100%'
+                        }}
+                        className="img-fluid"
+                        />
+
+                </Container>
+                <h2>Kosala Maduwantha</h2>
             </Row>
         </Col>
         <Col>
